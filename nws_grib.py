27f9134b39ea6f4a13d1891536b68740c7ae2001
Jody Sankey <jody@jsankey.com>
2022-05-25 23:49:18 -0700

    Much better (but still not HRRR) grib script.

--- nws_grib.py
+++ nws_grib.py
@@ -25,56 +25,38 @@ import tempfile
 import requests
 
 
-LEVELS = [
-    'surface',
-    'mean_sea_level',
-    '10_m_above_ground', # Used for surface wind
-    'entire_atmosphere', # Used for cloud cover
-    '500_mb',
-]
-
-VARIABLES = [
-    'UGRD',  # U component of wind
-    'VGRD',  # V component of wind
-    'GUST',  # Wind gust
-    'PRMSL',  # Pressure (for surface) [Not available in HRRR]
-    'HGT',   # Geopotential height (for 500mb)
-    'TMP',  # Temperature
-    'PRATE', # Precipitation rate
-    'VIS',   # Visibility
-    'TCDC',  # Total cloud cover
-]
-
-
 class DataProduct:
     """A representation of NOAA weather data product, able to make requests for data."""
-    def __init__(self, base_url, dir_param, file_param, output_prefix, interval):
+    def __init__(self, base_url, dir_fn, file_fn, out_prefix, interval, levels, variables):
         self.base_url = base_url
-        self.dir_param = dir_param
-        self.file_param = file_param
-        self.output_prefix = output_prefix
+        self.dir_fn = dir_fn
+        self.file_fn = file_fn
+        self.out_prefix = out_prefix
         self.interval = interval
+        self.levels = levels
+        self.variables = variables
 
     def _make_params(self, forecast_time, forecast_hour, args):
-        """Constructs a URL param dictionary for the requested time and forecast."""
+        """Constructs a URL paramater dictionary for the requested forecast time and an hour in
+        that forecast."""
         basics = {
-            'file': self.file_param.format(forecast_time.hour, forecast_hour),
-            'dir': self.dir_param.format(forecast_time.strftime('%Y%m%d'), forecast_time.hour),
+            'file': self.file_fn(forecast_time, forecast_hour),
+            'dir': self.dir_fn(forecast_time),
             'subregion': '',
             'leftlon': args.min_lon,
             'rightlon': args.max_lon,
             'toplat': args.max_lat,
             'bottomlat': args.min_lat,
         }
-        levels = {'lev_{}'.format(l): 'on' for l in LEVELS}
-        variables = {'var_{}'.format(v): 'on' for v in VARIABLES}
+        levels = {'lev_{}'.format(l): 'on' for l in self.levels}
+        variables = {'var_{}'.format(v): 'on' for v in self.variables}
         return {**basics, **levels, **variables}
 
     def request_forecast(self, forecast_time, forecast_hour, args):
         """Requests the URL for the requested time and forecast, printing an info message
         beforehand."""
-        print('Requesting data for {}, forecast hour {}'.format(
-            forecast_time.strftime('%Y%m%d %HZ'), forecast_hour))
+        print('Requesting {} data for {}, forecast hour {}'.format(
+            self.out_prefix, forecast_time.strftime('%Y%m%d %HZ'), forecast_hour))
         return requests.get(self.base_url,
                             params=self._make_params(forecast_time, forecast_hour, args))
 
@@ -90,24 +72,76 @@ class DataProduct:
 
     def filename(self, forecast_time):
         """Returns a convenient string for the supplied forecast datetime."""
-        return forecast_time.strftime(self.output_prefix + '%Y%m%d_%Hz.grb2')
+        return forecast_time.strftime(self.out_prefix + '_%Y%m%d_%Hz.grb2')
 
 
 PRODUCTS = {
     'GFS': DataProduct(
-        r'https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl',
-        r'/gfs.{}/{}/atmos',
-        r'gfs.t{:02d}z.pgrb2.0p25.f{:03d}',
-        r'gfs_0p25_',
-        6),
-    # Unfortunately HRRR isn't usable yet. zxgrib entirely fails to display the any grib from HRRR
+        base_url=r'https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl',
+        dir_fn=lambda time: r'/gfs.{}/{:02d}/atmos'.format(time.strftime('%Y%m%d'), time.hour),
+        file_fn=lambda time,hour: r'gfs.t{:02d}z.pgrb2.0p25.f{:03d}'.format(time.hour, hour),
+        out_prefix=r'gfs_0p25',
+        interval=6,
+        levels=[
+            'surface',
+            'mean_sea_level',
+            '2_m_above_ground', # Used for air temperature
+            '10_m_above_ground', # Used for surface wind
+            'entire_atmosphere', # Used for cloud cover
+            '500_mb',
+        ],
+        variables=[
+            'UGRD',  # U component of wind
+            'VGRD',  # V component of wind
+            'GUST',  # Wind gust
+            'PRMSL', # Pressure (for surface) [Not available in HRRR]
+            'HGT',   # Geopotential height (for 500mb)
+            'TMP',   # Temperature
+            'PRATE', # Precipitation rate
+            'VIS',   # Visibility
+            'TCDC',  # Total cloud cover
+        ]),
+    'GFSwavewcoast': DataProduct(
+        base_url=r'https://nomads.ncep.noaa.gov/cgi-bin/filter_gfswave.pl',
+        dir_fn=lambda time: r'/gfs.{}/{:02d}/wave/gridded'.format(
+            time.strftime('%Y%m%d'), time.hour),
+        file_fn=lambda time,hour: r'gfswave.t{:02d}z.wcoast.0p16.f{:03d}.grib2'.format(
+            time.hour, hour),
+        out_prefix=r'wave_wcoast_0p16',
+        interval=6,
+        levels=['surface'],
+        variables=[
+            'HTSGW',  # Significant wave height
+            'WVDIR',  # Wind wave direction
+            'WVPER',  # Wind wave period
+        ]),
+    # Unfortunately HRRR isn't usable yet. zxgrib entirely fails to display any grib from HRRR
     # while OpenCPN displays moderately beleivable data but on the wrong place on the map.
     'HRRR': DataProduct(
-        r'https://nomads.ncep.noaa.gov/cgi-bin/filter_hrrr_2d.pl',
-        r'/hrrr.{}/conus', # Note doesn't contain an hour, gonna need to do something different.
-        r'hrrr.t{:02d}z.wrfsfcf{:02d}.grib2',
-        r'hrrr_conus_',
-        1),
+        base_url=r'https://nomads.ncep.noaa.gov/cgi-bin/filter_hrrr_2d.pl',
+        dir_fn=lambda time: r'/hrrr.{}/conus'.format(time.strftime('%Y%m%d')),
+        file_fn=lambda time,hour: r'hrrr.t{:02d}z.wrfsfcf{:02d}.grib2'.format(time.hour, hour),
+        out_prefix=r'hrrr_conus',
+        interval=1,
+        levels=[
+            'surface',
+            'mean_sea_level',
+            '2_m_above_ground', # Used for air temperature
+            '10_m_above_ground', # Used for surface wind
+            'entire_atmosphere', # Used for cloud cover
+            '500_mb',
+        ],
+        variables=[
+            'UGRD',  # U component of wind
+            'VGRD',  # V component of wind
+            'GUST',  # Wind gust
+            #'PRMSL', # Pressure (for surface) [Not available in HRRR]
+            'HGT',   # Geopotential height (for 500mb)
+            'TMP',   # Temperature
+            'PRATE', # Precipitation rate
+            'VIS',   # Visibility
+            'TCDC',  # Total cloud cover
+        ]),
 }
 
 
@@ -140,30 +174,30 @@ def create_parser():
 def main():
     """Executes the script using command line arguments."""
     args = create_parser().parse_args()
-    product = PRODUCTS['GFS'] # Because HRRR doesn't work yet
-    forecast_time = product.get_most_recent_cycle_time()
-    resp = product.request_forecast(forecast_time, 0, args)
-
-    # If the server doesn't have this most recent time yet, backoff to the previous time
-    if resp.status_code == 404:
-        print('Not found, backing off one interval')
-        forecast_time = product.get_previous_cycle_time(forecast_time)
+    for product in [PRODUCTS[name] for name in ['GFS', 'GFSwavewcoast']]:
+        forecast_time = product.get_most_recent_cycle_time()
         resp = product.request_forecast(forecast_time, 0, args)
 
-    # If neither succeeded just quit.
-    if resp.status_code != 200:
-        sys.exit('HTTP response code {}'.format(resp.status_code))
+        # If the server doesn't have this most recent time yet, backoff to the previous time
+        if resp.status_code == 404:
+            print('Not found, backing off one interval')
+            forecast_time = product.get_previous_cycle_time(forecast_time)
+            resp = product.request_forecast(forecast_time, 0, args)
 
-    # Write this first response and a set of additional forecast hours to file.
-    filepath = os.path.join(args.output_dir, product.filename(forecast_time))
-    with open(filepath, 'wb') as file:
-        file.write(resp.content)
-        for forecast_hour in range(1, args.duration, args.interval):
-            resp = product.request_forecast(forecast_time, forecast_hour, args)
-            if resp.status_code != 200:
-                sys.exit('HTTP response code {}'.format(resp.status_code))
+        # If neither succeeded just quit.
+        if resp.status_code != 200:
+            sys.exit('HTTP response code {}'.format(resp.status_code))
+
+        # Write this first response and a set of additional forecast hours to file.
+        filepath = os.path.join(args.output_dir, product.filename(forecast_time))
+        with open(filepath, 'wb') as file:
             file.write(resp.content)
-            time.sleep(args.sleep / 1000.0)
+            for forecast_hour in range(1, args.duration, args.interval):
+                resp = product.request_forecast(forecast_time, forecast_hour, args)
+                if resp.status_code != 200:
+                    sys.exit('HTTP response code {}'.format(resp.status_code))
+                file.write(resp.content)
+                time.sleep(args.sleep / 1000.0)
         print('Wrote {}'.format(filepath))
 
 
