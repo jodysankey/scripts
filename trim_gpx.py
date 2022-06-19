e4fbd626f66f2069d826719b8a6b09df5b184c3a
Jody Sankey <jody@jsankey.com>
2022-06-19 10:06:37 -0700

    Update trim_gpx to handle non-Navionics files.

--- trim_gpx.py
+++ trim_gpx.py
@@ -165,9 +165,13 @@ class Track:
 
     def trim(self, start, end):
         """Delete all points outside the supplied indices and update times to match."""
+        # Update the custom Navionics time propertied if they exist.
         ext = self.xml_segment.find('extensions', NS)
-        ext.find('navionics_start_time', NS).text = self.points[start].xml.find('time', NS).text
-        ext.find('navionics_end_time', NS).text = self.points[end].xml.find('time', NS).text
+        if ext and ext.find('navionics_start_time', NS):
+            ext.find('navionics_start_time', NS).text = self.points[start].xml.find('time', NS).text
+        if ext and ext.find('navionics_end_time', NS):
+            ext.find('navionics_end_time', NS).text = self.points[end].xml.find('time', NS).text
+        # Delete any points outside the time range.
         start_time = self.points[start].time
         end_time = self.points[end].time
         for point in self.points:
